e39955351a68a7556dfd0a27f634f8d76e33d8cc
armandoherrera <herreraarmando874@gmail.com>
2021-12-04 16:39:29 -0500

    Se agrega el Proyecto

---
+++ src/Data2.js
@@ -0,0 +1,117 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "FUNBACTER X LITRO",
+      price: 401,
+      image: "https://mundocampo.com.co/wp-content/uploads/2020/06/xsC_Mesa-de-trabajo-1.png",
+      category: "Proseagro",
+      //img1: "https://mundocampo.com.co/wp-content/uploads/2020/06/xsC_Mesa-de-trabajo-1.png",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      title: "Semilla de Maíz Híbrido Somma",
+      price: 304,
+      image: "https://www.e-agrizon.com/wp-content/uploads/2019/11/Semilla-de-Maiz-Hibrido-Somma.jpg",
+      category: "Proseagro",
+      //img1: "https://www.e-agrizon.com/wp-content/uploads/2019/11/Semilla-de-Maiz-Hibrido-Somma.jpg",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1606319825&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "Semilla de Frijol cargamanto rojo x 1 kg",
+      price: 475,
+      image: "https://cdn.shopify.com/s/files/1/0410/2199/7206/products/frijol-cargamanto-rojo-impulsem-x-1-kg_5dcd64e0-cfa9-4e69-8bd2-6f737ea7b2f4_500x.jpg?v=1625172643",
+      category: "Proseagro",
+      //img1: "https://stockx-360.imgix.net/Nike-Air-Force-1-Low-Supreme-Box-Logo-Black/Images/Nike-Air-Force-1-Low-Supreme-Box-Logo-Black/Lv2/img",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1606325289&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "CompostCampo",
+      price: 399,
+      image: "https://cdn.shopify.com/s/files/1/0410/2199/7206/products/CompostCampo__40_kg_500x.png?v=1624545746",
+      category: "Proseagro",
+      //img1: "",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "FertiEspecial",
+      price: 405,
+      image: "https://cdn.shopify.com/s/files/1/0410/2199/7206/products/110877-Fertiespecial46kg_500x.png?v=1596302210",
+      category: "Proseagro",
+      //img1: "",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1606322330&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "HidroKofi",
+      price: 285,
+      image: "https://cdn.shopify.com/s/files/1/0410/2199/7206/products/111661-Hidrokofi25kg_500x.png?v=1624545711",
+      category: "Proseagro",
+      //img1: "",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1606321824&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 7,
+      title: "Furtivo 250 sc",
+      price: 360,
+      image: "https://cdn.shopify.com/s/files/1/0410/2199/7206/products/Furtivo_1Lt-600x600_500x.png?v=1603920132",
+      category: "Proseagro",
+      //img1: "",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1606320966&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 8,
+      title: "Alguacil 250 sc",
+      price: 335,
+      image: "https://cdn.shopify.com/s/files/1/0410/2199/7206/products/Alguacil_1Lt-600x600_500x.png?v=1603919335",
+      category: "Proseagroe",
+      //img1: "",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1606325265&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 9,
+      title: "Fertinvesa K SC x",
+      price: 799,
+      image: "https://cdn.shopify.com/s/files/1/0410/2199/7206/products/fertinvesa-k-1-lt_ef4197bc-dfad-4551-a9b3-2c260e804544_500x.jpg?v=1596331752",
+      category: "Proseagro",
+      //img1: "",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1606323250&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 10,
+      title: "Motosierra Montana 1620",
+      price: 501,
+      image: "https://www.e-agrizon.com/wp-content/uploads/2020/06/garland-motosierra-montana-1620..-600x600.jpg",
+      category: "Proseagro",
+      //img1: "",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1609445259&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 11,
+      title: "Motoguadaña a gasolina",
+      price: 377,
+      image: "https://www.e-agrizon.com/wp-content/uploads/2019/11/Motoguada%C3%B1a-a-gasolina-2-600x600.png",
+      category: "Proseagro",
+      //img1: "",
+      //img2: ".jpg?auto=format,compress&q=90&updated_at=1609438911&w=1000",
+      cantidad: 1
+    },
+  ],
+};
+
+export default data;
